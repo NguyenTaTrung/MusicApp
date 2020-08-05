03b48c7c22ac83cbcea2cfd19437f6f1c9c7974a
TrungNT83 <trungnt83@fsoft.com.vn>
2020-08-05 10:22:59 +0700

    first commit

---
+++ app/src/main/java/sun/trainingcourse/musicapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package sun.trainingcourse.musicapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
